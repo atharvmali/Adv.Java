daa5801879e6db5f07967722807c02c2f73939fe
atharvmali <atharv.mali.1215@gmail.com>
2024-09-02 07:56:29 +0530

    demo indentation update

--- src/Demo.java
+++ src/Demo.java
@@ -5,18 +5,25 @@ public class Demo {
         Connection con;
         Statement st;
         ResultSet rs;
+        
         try {
+        	
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/college", "root", "mysqlroot");
             st = con.createStatement();
             rs = st.executeQuery("select * from student");
+            
             while (rs.next()) {
                 int id = rs.getInt("rollno");
+                
                 String nm = rs.getString("sname");
+                
                 String cls = rs.getString("course");
+                
                 System.out.println("ID: " + id + " Name: " + nm + " Course: " + cls);
             }
             con.close();
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
